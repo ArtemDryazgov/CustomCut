ca8f54b3aa07c45a63a02c6f3c9a78f0cb9f94bf
Artem Dryazgov <artomdrazgov@gmail.com>
2021-05-07 00:14:22 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/* Скрипт слайдера */
+const slider = tns({
+	container: '.gallery_slide',
+	items: 2,
+	slideBy: 'page',
+	controls: false,
+	navPosition: "bottom",
+	speed: 1500,
+	responsive: {
+		340: {
+			items: 1,
+		},
+		470: {
+			nav: true,
+		},
+		767: {
+			items: 2,
+			nav: true,
+		}
+	}
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+	const menu = document.querySelector('.nav__list'),
+		menuItem = document.querySelectorAll('.menu_item'),
+		hamburger = document.querySelector('.hamburger');
+
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger_active');
+		menu.classList.toggle('nav__list_active');
+	});
+
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('nav__list_active');
+		})
+	})
+})
